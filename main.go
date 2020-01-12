de82c75e08398583053c4f697e5cdf5c561e72d2
cszczepaniak <cszczepaniak@gmail.com>
2020-01-12 15:47:45 -0600

    parse stuff better

--- main.go
+++ main.go
@@ -3,22 +3,38 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/cszczepaniak/cribbage-scorer/cards"
+	"github.com/cszczepaniak/cribbage-scorer/score"
+)
+
+var (
+	cutStr = flag.String("cut", "", "the cut card")
+	isCrib = flag.Bool("iscrib", false, "whether or not this is a crib")
 )
 
 func main() {
-	hand := make([]cards.Card, 0, 5)
-
-	cutPtr := flag.String("cut", "", "the cut card")
+	start := time.Now()
+	hand := make([]cards.Card, 0, 4)
 	flag.Parse()
-
-	clargs := flag.Args()
-	for _, code := range clargs {
-		hand = append(hand, cards.CardMap[code])
+	args := flag.Args()
+	for _, a := range args {
+		c, err := cards.NewCardFromString(a)
+		if err != nil {
+			log.Fatal(err)
+		}
+		hand = append(hand, c)
 	}
-
-	myHand := cards.Hand{Cards: hand, Cut: cards.CardMap[*cutPtr], IsCrib: false}
-	fmt.Println(myHand.AllCards())
-	fmt.Println("Score:", cards.ScoreHand(myHand))
+	cut, err := cards.NewCardFromString(*cutStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s, err := score.ScoreHand(hand, cut, *isCrib)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Score is: %d\n", s)
+	fmt.Printf("time elapsed: %s", time.Since(start))
 }
