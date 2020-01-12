2e5b2b24afe9c5aaba792267c4eedba4119f9706
cszczepaniak <cszczepaniak@gmail.com>
2020-01-12 11:33:15 -0600

    don't need to use the empty interface

--- score/score.go
+++ score/score.go
@@ -19,15 +19,11 @@ func scoreFifteens(hand []cards.Card, cut cards.Card, isCrib bool) (int, error) 
 	all := append(hand, cut)
 	score := 0
 	for i := 2; i < 6; i++ {
-		combs := comb.Combinations(hand, i)
+		combs := comb.Combinations(all, i)
 		for _, comb := range combs {
 			val := 0
 			for _, c := range comb {
-				card, ok := c.(cards.Card)
-				if !ok {
-					return 0, errors.New(`type assertion failed`)
-				}
-				val += card.Value()
+				val += c.Value()
 			}
 			if val == 15 {
 				score += 2
