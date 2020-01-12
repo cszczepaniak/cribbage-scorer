44a217e2959c3ceaf18dea25c7fd47d4c5a7d53d
cszczepaniak <cszczepaniak@gmail.com>
2020-01-12 15:56:52 -0600

    delete unused stuff

--- comb/comb.go
+++ comb/comb.go
@@ -4,30 +4,6 @@ import (
 	"github.com/cszczepaniak/cribbage-scorer/cards"
 )
 
-var cache map[int]int
-
-func init() {
-	cache = make(map[int]int)
-}
-
-func Factorial(n int) int {
-	res, ok := cache[n]
-	if ok {
-		return res
-	}
-	if n <= 1 {
-		res = 1
-	} else {
-		res = n * Factorial(n-1)
-	}
-	cache[n] = res
-	return res
-}
-
-func Nchoosek(n, k int) int {
-	return Factorial(n) / (Factorial(k) * Factorial(n-k))
-}
-
 func Combinations(superset []cards.Card, n int) [][]cards.Card {
 	if len(superset) == n {
 		return [][]cards.Card{superset}
