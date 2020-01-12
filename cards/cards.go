66248fc0cbeb8e9a81add5cdbe490aeb18af1ad7
cszczepaniak <cszczepaniak@gmail.com>
2020-01-12 11:03:14 -0600

    refactor cards

--- cards/cards.go
+++ cards/cards.go
@@ -1,20 +1,94 @@
 package cards
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"errors"
+	"strings"
 )
 
-var AllCards []Card
-var CardMap = make(map[string]Card)
+var (
+	ErrInvalidCardString = errors.New(`invalid card string`)
+	ErrInvalidSuit       = errors.New(`invalid suit`)
+	ErrInvalidRank       = errors.New(`invalid rank`)
+)
+
+type Suit int
+
+const (
+	Clubs Suit = iota
+	Diamonds
+	Hearts
+	Spades
+)
 
 type Card struct {
-	Suit  string `json:"suit"`
-	Value int    `json:"value"`
-	Rank  int    `json:"rank"`
-	Name  string `json:"name"`
-	Code  string `json:"code"`
+	Suit Suit   `json:"suit"`
+	Rank int    `json:"rank"`
+	Name string `json:"name"`
+}
+
+func NewCardFromString(s string) (Card, error) {
+	rank, err := rankFromString(s)
+	if err != nil {
+		return Card{}, err
+	}
+	suit, err := suitFromString(s)
+	if err != nil {
+		return Card{}, err
+	}
+	return Card{
+		Suit: suit,
+		Rank: rank,
+		Name: s,
+	}, nil
+}
+
+func suitFromString(s string) (Suit, error) {
+	s = strings.ToLower(s)
+	runes := []rune(s)
+	if len(runes) > 3 || len(runes) < 2 {
+		return -1, ErrInvalidCardString
+	}
+	switch runes[len(runes)-1] {
+	case 'c':
+		return Clubs, nil
+	case 'd':
+		return Diamonds, nil
+	case 'h':
+		return Hearts, nil
+	case 's':
+		return Spades, nil
+	default:
+		return -1, ErrInvalidSuit
+	}
+}
+
+func rankFromString(s string) (int, error) {
+	s = strings.ToLower(s)
+	runes := []rune(s)
+	if len(runes) > 3 || len(runes) < 2 {
+		return 0, ErrInvalidCardString
+	}
+	if len(runes) == 3 {
+		if runes[0] == '1' && runes[1] == '0' {
+			return 10, nil
+		}
+		return 0, ErrInvalidRank
+	}
+	switch runes[0] {
+	case 'a':
+		return 1, nil
+	case 'j':
+		return 11, nil
+	case 'q':
+		return 12, nil
+	case 'k':
+		return 13, nil
+	default:
+		if runes[0] < '2' || runes[0] > '9' {
+			return 0, ErrInvalidRank
+		}
+		return int(runes[0] - '0'), nil
+	}
 }
 
 type Hand struct {
@@ -23,19 +97,6 @@ type Hand struct {
 	IsCrib bool
 }
 
-func (h Hand) AllCards() []Card {
-	return append(h.Cards, h.Cut)
-}
-
-func init() {
-	dat, _ := ioutil.ReadFile("cards.json")
-	json.Unmarshal(dat, &AllCards)
-
-	for _, card := range AllCards {
-		CardMap[card.Code] = card
-	}
-}
-
-func (c Card) Print() {
-	fmt.Println(c.Name, "of", c.Suit)
+func (c Card) String() string {
+	return c.Name
 }
