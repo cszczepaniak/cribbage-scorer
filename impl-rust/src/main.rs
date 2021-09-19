f2f46354a48132b8cf9da1e56a50b7df4d2d150e
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-09-19 17:23:26 -0500

    implement scoring

--- impl-rust/src/main.rs
+++ impl-rust/src/main.rs
@@ -1,3 +1,27 @@
+use cribbage_scorer::cards::Card;
+use itertools::Itertools;
+
 fn main() {
-    println!("Hello, world!");
+    let mut num = 0;
+    for hand in cribbage_scorer::cards::new_deck()
+        .into_iter()
+        .combinations(5)
+    {
+        for _ in hands_from_cards(&hand) {
+            num += 1
+        }
+    }
+    println!("{:?}", num);
+}
+
+fn hands_from_cards<'a>(h: &'a [Card]) -> Vec<(Card, Vec<&'a Card>)> {
+    let mut res = Vec::with_capacity(5);
+    for i in 0..h.len() {
+        let cut = h[i];
+        let mut rest: Vec<&Card> = h.into_iter().take(i).collect();
+        let end = h.into_iter().skip(i + 1);
+        rest.extend(end);
+        res.push((cut, rest));
+    }
+    res
 }
